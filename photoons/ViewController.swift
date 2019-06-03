1fc101994cffd6f0b26b7c526f8c07ea33a394d1
Pinaworld <pinben02@gmail.com>
2019-06-03 12:22:05 +0200

    FirstOne

---
+++ photoons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photoons
+//
+//  Created by etudiant on 03/06/2019.
+//  Copyright © 2019 benoit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func registerBut(_ sender: Any) {
+        
+    }
+    
+    @IBAction func connectGoogle(_ sender: Any) {
+    }
+}
+
